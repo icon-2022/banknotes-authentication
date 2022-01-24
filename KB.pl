ab413956464c19f80963138e63856675f5667fea
lucazeverino <lucazeverino@gmail.com>
2022-01-24 22:32:52 +0100

    Aggiornamento progetto

--- KB.pl
+++ KB.pl
@@ -6,14 +6,14 @@ prop(X, type, C) :- prop(S, subClassOf, C), prop(X, type, S).
 prop(E, lista_pazienti, true):- prop(E, type, paziente), prop(E, occupancy, N), N >= 0.
 
 
-% PAZIENTI
+% Pazienti
 prop(sottopeso_estremo, type, paziente).
 prop(sottopeso, type, paziente).
 prop(normopeso, type, paziente).
 prop(sovrappeso, type, paziente).
 prop(obeso, type, paziente).
 
-% POSTI OCCUPATI ALL'INTERNO DELL'AMBULATORIO
+% Posti occupati all'interno dell'ambulatorio
 prop(sottopeso_estremo, occupancy, 7).
 prop(sottopeso, occupancy, 5).
 prop(normopeso, occupancy, 2).
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import prediction as pr
-import classifier as cl
 import pazienti as pz
 
 pz.nuovoPaziente()
 pz.listaPazienti()
-#cl.domanda()
-#pr.prediction()
